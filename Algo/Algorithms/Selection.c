b8b409b24d1a8b9efbedd96927b445b461af8668
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-02-09 14:22:06 +0600

    Algorithms

---
+++ Algo/Algorithms/Selection.c
@@ -0,0 +1,37 @@
+// Selection Sort.
+
+#include<stdio.h>
+int main()
+{
+    int ara[100], n, t, min;
+
+    printf("Enter Length of an array : ");
+    scanf("%d", &n);
+
+    printf("\nEnter Values of the array : ");
+    for(int i=0; i<n; i++){
+          scanf("%d", &ara[i]);
+    }
+
+    for(int i=0; i<n-1; i++){
+        min = i;
+
+        for(int j=i+1; j<n; j++){
+            if(ara[j] < ara[min]){
+                min = j;
+            }
+        }
+        if(min!=i){
+            t = ara[i];
+            ara[i] = ara[min];
+            ara[min] = t;
+        }
+    }
+
+    printf("\nAfter Selection Sorted : ");
+    for(int i=0; i<n; i++){
+            printf("%d ",ara[i]);
+    }
+    printf("\n");
+    return 0;
+}
