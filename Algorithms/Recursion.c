47ff093706cf2173642ed3f548dc3a8c6e1909b5
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-02-09 20:25:01 +0600

    A simple recursion program

---
+++ Algorithms/Recursion.c
@@ -0,0 +1,31 @@
+/*
+    Recursion: Recursion is a process in which a function calls itself
+    directly or indirectly.
+
+    Example:
+           int fun()
+           {
+              ....
+              fun();
+           }
+
+    Output: 3
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n = 3;
+    printf("%d", fun(n)); // Function Call.
+    return 0;
+}
+
+// Function is fun().
+int fun(int n)
+{
+    if (n == 1)
+        return 1;  // return 1 means it returns the 1 times in the function.
+    else
+        return 1 + fun(n - 1);
+}
